f866c0758d66d644b69861ed25c1e1fdcebb9f37
RohitBisht <rohitbisht@PL-Rohit-MACBOOK-Q72R6G774X.local>
2025-08-21 13:42:48 +0530

    chore: initial project setup

---
+++ .eslintec.cjs
@@ -0,0 +1,17 @@
+  module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-refresh'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  ignorePatterns: ['dist', 'node_modules'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }]
+  }
+};
